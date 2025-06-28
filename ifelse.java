c967d149af7a997257fe4fe8828fad84ab226476
SabarieaswaranG <146971440+SabarieaswaranG@users.noreply.github.com>
2025-06-28 15:43:36 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,164 @@
+// // import java.util.Scanner;
+
+// // public class ifelse {
+// //     public static void main(String[] args) {
+// //         boolean rain=false;
+// //         if(rain)
+// //         {
+// //             System.out.println("take umbrella");
+
+// //         }
+// //         else{
+// //             System.out.println("enjoy sunlight");
+// //         }
+// //     }
+    
+// // }
+
+// // import java.util.Scanner;
+
+// // class bank{
+// //     public static void main(String[] args) {
+// //         Scanner scan=new Scanner(System.in);
+// //         System.out.println("enter your salary :");
+// //         int salary=scan.nextInt();
+// //         System.out.println("enter you age :");
+// //         int age=scan.nextInt();
+// //         if(salary>=20000|| age<=25){
+// //             System.out.println("enter the loan amount :");
+// //             int loan=scan.nextInt();
+// //             if(loan<=50000){
+// //                 System.out.println("loan amount eligible");
+// //             }
+// //             else{
+// //                 System.out.println("maximum loan amount 50000");
+// //             }
+// //         }
+// //         else{
+// //             System.out.println("not eligible for loan");
+// //         }
+
+// //     }
+// // }
+
+// // import java.util.Scanner;
+
+// // class saba{
+// //     public static void main(String[] args) {
+// //         Scanner scan=new Scanner(System.in);
+// //         System.out.println("enter your password");
+// //         String name=scan.nextLine();
+// //         String password=(name=="arun2003")?"password is correct":"password is incorrect";
+// //         System.out.println(password);
+// //     }
+// // // }
+// // // import java.util.Scanner;
+
+// // import java.util.Scanner;
+// // class sanjai{
+// //     public static void main(String[] args) {
+// //         Scanner scan=new Scanner (System.in);
+// // int a=scan.nextInt();
+// // if(a%2==0){
+// // System.out.println("a is Even");
+// //     }
+
+// //  else{
+// // System.out.println("a is oddSystem.out.print(\"Enter any value\");");
+// //  }
+// //  }
+// // }
+
+    
+
+
+
+
+// // import java.util.Scanner;
+// // class sanjai{
+// //     public static void main(String[] args) {
+
+// //         Scanner scan=new Scanner (System.in);
+// // System.out.println("Enter  any year");
+// //         int year=scan.nextInt();
+// // if(year%4==0){
+// // System.out.println(year+"it is leap year");
+// //     }
+
+// //  else{
+// // System.out.println(year+"it is  not leap year");
+// //  }
+// //  }
+// // }
+
+// // import java.util.Scanner;
+
+// // class saba{
+// //     public static void main(String[] args) {
+        
+// //         int i=1;
+// //         for(i=1;i<=15;i=i+3){
+// //             System.out.println(i);
+
+// //         }
+// //     }
+// // }
+
+// // import java.util.Scanner;
+
+// // class saba{
+// //     void sum(int a,int b){
+// //         System.out.println(a+b);
+// //     }
+    
+
+// // }
+// // public static void main(String[] args){
+// //     Scanner saba=new Scanner(System.in);
+// //     System.out.println("Enter the a value");
+    
+// import java.util.Scanner;
+// class saba{
+//     public static void main(String[] args) {
+//         Scanner scan=new Scanner(System.in);
+//         System.out.println("Enter your mark");
+//         int score=scan.nextInt();
+//         if(score>35 && score<=60 ){
+//             System.out.println("vidio game");
+//         }
+//         else if(score>60 && score<=90){
+//             System.out.println("iphone");
+
+//         }
+//         else if(score>90){
+//             System.out.println("mac");
+//         }
+//         else{
+//             System.out.println("mmbu");
+//         }
+//     }
+// }
+
+// // class saba{
+// //     public static void main(String[] args) {
+// //         Scanner scan=new Scanner(System.in);
+// //         int a=scan.nextInt();
+// //         if(a%3==0){
+// //             System.out.println("a is divisible by 3");
+// //         }
+// //         else{
+// //             System.out.println("a is not divisible");
+// //         }
+
+// //     }
+// // import java.util.Scanner;
+// // class arun
+// // {
+// //     public static void main(String[] args) {
+// //         Scanner ak=new Scanner(System.in);
+// //         System.out.print("Enter any value :");
+// //         int num=ak.nextInt();
+// //         String result=(num%5==0)?"number is divisible":"not divisible";
+// //         System.out.print(result);
+// //     }
+// // }
