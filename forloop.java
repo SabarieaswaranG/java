4dfbf65d269982da930a0518488096a88ec807a9
SabarieaswaranG <146971440+SabarieaswaranG@users.noreply.github.com>
2025-06-28 15:48:41 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,72 @@
+// // class saba {
+// //     public static void main(String[] args) {
+// //         int i=0;
+// //         for(i=1;i<=10;i=i+1){
+// //             System.out.println(i);
+// //         }
+// //     }
+    
+// // }
+
+
+
+// // class saba {
+// //     public static void main(String[] args) {
+// //         for(int i=10;i>=1;i=i-1){
+// //         System.out.println(i);
+// //         }
+// //     }
+    
+// // }
+
+// import java.util.Scanner;
+
+// // class saba{
+// //     public static void main(String[] args) {
+// //         Scanner scan=new Scanner(System.in);
+// //         System.out.println("number 1");
+// //         int a=scan.nextInt();
+// //         System.out.println("number 2");
+// //         int b=scan.nextInt();
+// //         System.out.println("your order");
+// //         for(int i=a;i<=b;i=i+1){
+// //             System.out.println(i);
+// //         }
+// //     }
+
+// //     }
+
+// // class saba{
+// //     public static void main(String[] args) {
+// //         Scanner scan=new Scanner(System.in);
+// //         System.out.println("number 1");
+// //         int a=scan.nextInt();
+// //         System.out.println("number 2");
+// //         int b=scan.nextInt();
+// //         System.out.println("your order");
+// //         for(int i=a;i<=100;i=i+2){
+// //         System.out.println("even no"+i);
+// //         }
+// //     }
+
+// //     }
+
+
+// class saba{
+//     public static void main(String[] args) {
+//         Scanner scan=new Scanner(System.in);
+//         int a=scan.nextInt();
+//         int b=scan.nextInt();
+//         for(int i=a;i<=b;i=i+2){
+//             System.out.println(i);
+            
+//         }
+//         if(a%2==0){
+//             System.out.println("even numbers");
+//         }
+//         else{
+//             System.out.println("odd numbers are");
+//         }
+    
+//     }
+// }
